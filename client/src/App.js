919fd98621efda661064a371037d5c6612879a34
raccoon0906 <lakun7157@gmail.com>
2024-01-13 18:07:18 +0900

    add customers

--- client/src/App.js
+++ client/src/App.js
@@ -21,34 +21,26 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id' : 1,
-  'image': 'https://tse1.mm.bing.net/th?id=OIP.YQD7SQcCtCGg2c39FjzBmAHaGU&pid=Api&P=0&h=220',
-  'name' : '동동준나예유',
-  'birthday' : '20060906',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image': 'https://tse1.mm.bing.net/th?id=OIP.YQD7SQcCtCGg2c39FjzBmAHaGU&pid=Api&P=0&h=220',
-  'name' : '코로나',
-  'birthday' : '19990101',
-  'gender' : '트렌스젠더',
-  'job' : '백수'
-}
-,{
-  'id' : 3,
-  'image': 'https://tse1.mm.bing.net/th?id=OIP.YQD7SQcCtCGg2c39FjzBmAHaGU&pid=Api&P=0&h=220',
-  'name' : '고무',
-  'birthday' : '20000101',
-  'gender' : '놈자',
-  'job' : '여포'
-}
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+
+
   render() { 
     const { classes } = this.props;
     return (
@@ -65,7 +57,8 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-          {customers.map(c => {
+          {this.state.customers ?
+          this.state.customers.map(c => {
             return (
               <Customer
                 key={c.id}
@@ -77,7 +70,7 @@ class App extends Component {
                 job ={c.job}
               />
             )
-          })}
+          }) : ""}
           </TableBody>
           </Table>
     </Paper>
--- server.js
+++ server.js
@@ -6,9 +6,34 @@ const port = process.env.PORT || 5100;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 
-app.get('/api/hello', (req, res) => {
-  res.send({message: 'Hello Express!'});
-})
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id' : 1,
+      'image': 'https://tse1.mm.bing.net/th?id=OIP.YQD7SQcCtCGg2c39FjzBmAHaGU&pid=Api&P=0&h=220',
+      'name' : '동동준나예유',
+      'birthday' : '20060906',
+      'gender' : '남자',
+      'job' : '대학생'
+    },
+    {
+      'id' : 2,
+      'image': 'https://tse1.mm.bing.net/th?id=OIP.YQD7SQcCtCGg2c39FjzBmAHaGU&pid=Api&P=0&h=220',
+      'name' : '코로나',
+      'birthday' : '19990101',
+      'gender' : '트렌스젠더',
+      'job' : '백수'
+    }
+    ,{
+      'id' : 3,
+      'image': 'https://tse1.mm.bing.net/th?id=OIP.YQD7SQcCtCGg2c39FjzBmAHaGU&pid=Api&P=0&h=220',
+      'name' : '고무',
+      'birthday' : '20000101',
+      'gender' : '놈자',
+      'job' : '여포'
+    }
+  ]);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
