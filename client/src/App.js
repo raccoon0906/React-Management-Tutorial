9ae2fee2f89fa945b635d9a0101d8e7879c78f6e
raccoon0906 <lakun7157@gmail.com>
2024-01-14 16:03:37 +0900

    loading view

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 
@@ -18,16 +19,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -39,7 +45,10 @@ class App extends Component {
     return body;
   }
 
-
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 })
+  }
 
   render() { 
     const { classes } = this.props;
@@ -70,7 +79,13 @@ class App extends Component {
                 job ={c.job}
               />
             )
-          }) : ""}
+          }) : 
+          <TableRow>
+            <TableCell colSpan="6" align='center'>
+              <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+          }
           </TableBody>
           </Table>
     </Paper>
